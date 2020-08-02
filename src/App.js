9ce40face0afda8d4a1e18b9294f7fc62299d13e
yn <Moreseus@outlook.com>
2020-08-03 04:37:00 +0700

    Fix Bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 import "./App.css";
 import MainPage from "./pages/MainPage";
 import ConfigPage from "./pages/ConfigPage";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { randomData } from "./data/randomData";
 
 const App = () => {
@@ -13,13 +13,15 @@ const App = () => {
   };
   return (
     <BrowserRouter basename="https://yunoyunoyuno.github.io/YN-RandomNumber/">
-      <Route
-        path="/setting"
-        render={(props) => (
-          <ConfigPage fakeDB={DUMMY_DATA.current} {...props} setDummyData={setDummyData} />
-        )}
-      />
-      <Route path="/" render={(props) => <MainPage fakeDB={DUMMY_DATA.current} {...props} />} />
+      <Switch>
+        <Route
+          path="/setting"
+          render={(props) => (
+            <ConfigPage fakeDB={DUMMY_DATA.current} {...props} setDummyData={setDummyData} />
+          )}
+        />
+        <Route path="/" render={(props) => <MainPage fakeDB={DUMMY_DATA.current} {...props} />} />
+      </Switch>
     </BrowserRouter>
   );
 };
