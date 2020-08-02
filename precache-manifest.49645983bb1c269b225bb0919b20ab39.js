540183aa5278c508b52032d4255de4867dfe7e3f
yn <Moreseus@outlook.com>
2020-08-03 06:18:35 +0700

    Updates

---
+++ precache-manifest.49645983bb1c269b225bb0919b20ab39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a9a80bbecc4fffb8fe99e09e974bff",
+    "url": "/randomgame/index.html"
+  },
+  {
+    "revision": "9462e83a80d5de316111",
+    "url": "/randomgame/static/css/main.9485cea0.chunk.css"
+  },
+  {
+    "revision": "8a98327af4ac7813eae8",
+    "url": "/randomgame/static/js/2.4410fe19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/randomgame/static/js/2.4410fe19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9462e83a80d5de316111",
+    "url": "/randomgame/static/js/main.5a2e8bde.chunk.js"
+  },
+  {
+    "revision": "656a9468ae6f9900e08e",
+    "url": "/randomgame/static/js/runtime-main.d0fb4348.js"
+  },
+  {
+    "revision": "19ad278675393aba977501c41d770d50",
+    "url": "/randomgame/static/media/Math.19ad2786.png"
+  }
+]);
